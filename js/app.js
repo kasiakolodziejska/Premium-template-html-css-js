926cf02b5ab9e055c98ba407bce2ac7ea0868899
Kasia <120297090+kasiakolodziejska@users.noreply.github.com>
2023-11-29 11:21:54 +0100

    Added files to repository

---
+++ js/app.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+
+    //HERO SLIDER
+    $(' #hero-slider').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        items: 1,
+        dots: false,
+        smartSpeed: 1000,
+        navText: ['<', '>'],
+        responsive:{
+            0:{
+               nav: false,
+            },
+            768:{
+                nav: true,
+            }
+           
+        }
+    });
+
+    //PROJECT SLIDER
+    $(' #project-slider').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        dots: false,
+        smartSpeed: 1000,
+        margin: 24,
+        navText: ["<",">"],
+        responsive:{
+            0:{
+               items: 1,
+               nav: false,
+               margin: 0,
+            },
+            768:{
+                items: 2,
+                
+            },
+            1140: {
+                items: 2,
+                center: true,
+            }
+        } 
+    });
+   
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:100,
+        nav: true,
+        dots: true,
+        items: 1,
+        center: true,
+        smartSpeed: 800,
+    })
+})
